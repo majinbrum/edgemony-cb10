70ccc9ac348a035759c54a28d85eaf566edaf34b
majinbru <majinbrum@gmail.com>
2024-06-25 17:30:00 +0200

    esercitazione-25-giugno

---
+++ 8_esercitazioni-week8/esercitazione-25-giugno/react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header.jsx";
+import { Main } from "./components/Main.jsx";
+import { Aside } from "./components/Aside.jsx";
+import { Footer } from "./components/Footer.jsx";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Main />
+			<Aside />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
---
+++ 8_esercitazioni-week8/esercitazione-25-giugno/react-app/src/components/Aside.jsx
@@ -0,0 +1,9 @@
+function Aside() {
+	return (
+		<aside>
+			<img src='https://picsum.photos/100/600' alt='Background' width='100' height='600' />
+		</aside>
+	);
+}
+
+export { Aside };
