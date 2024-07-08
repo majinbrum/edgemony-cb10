9dbebfcc354c17029785767c6278b38060681219
majinbru <majinbrum@gmail.com>
2024-07-08 13:14:13 +0200

    completed base project logic

--- 10_esercitazioni-week10/esercitazione-08-luglio/react-todo-list/src/App.jsx
+++ 10_esercitazioni-week10/esercitazione-08-luglio/react-todo-list/src/App.jsx
@@ -1,14 +1,57 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from "./App.module.css";
+
+const initialTodos = [
+	{
+		id: 1,
+		title: "Fare la spesa",
+	},
+	{
+		id: 2,
+		title: "Rinnovare assicurazione",
+	},
+];
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [todos, setTodos] = useState(initialTodos);
+	const [newTodo, setNewTodo] = useState({ title: "" });
+
+	const addTodo = () => {
+		if (!newTodo.title.length) return;
+
+		setTodos([...todos, newTodo]);
+		setNewTodo({ title: "" });
+	};
+
+	const updateTitle = (e) => {
+		const id = todos[todos.length - 1].id + 1;
+		const title = e.target.value;
+		setNewTodo({ id: id, title: title });
+	};
+
+	const deleteTodo = (todoToDelete) => {
+		const newTodoList = todos.filter((todo) => todo.id !== todoToDelete);
+		setTodos(newTodoList);
+	};
 
 	return (
 		<>
-			<p>Click on the Vite and React logos to learn more</p>
+			<div className={styles.container}>
+				<input type='text' placeholder='ToDo' value={newTodo.title} onChange={updateTitle} />
+				<button onClick={addTodo}>Add</button>
+
+				<div className={styles.container__list}>
+					<h2>List di Todo</h2>
+					<ul>
+						{todos.map((todo) => (
+							<li key={todo.id}>
+								{todo.title}
+								<button onClick={() => deleteTodo(todo.id)}>Delete</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 }
