e254547425f7cc833e9b4d3ac8ec9ce2740bf6ce
majinbru <majinbrum@gmail.com>
2024-08-29 01:44:20 +0200

    esercitazione-26-agosto

---
+++ 13_esercitazioni-week13/esercitazione-26-agosto/conta-caratteri/brief.js
@@ -0,0 +1,5 @@
+// * Esercizio 2 - Livello Difficile (Facoltativo)
+// * Crea un conta caratteri in JavaScript.
+// Il conta caratteri dovrebbe essere utilizzabile sia da terminale che in un'applicazione HTML o React. Deve contare i caratteri di una stringa, inclusi gli spazi. Se lo desideri, puoi aggiungere funzionalità extra, come il conteggio dei caratteri escludendo gli spazi.
+
+// esempio: ContaCaratteri.it
